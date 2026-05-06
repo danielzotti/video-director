5f0af887b810b921e66017e506925b47a6111c80
Daniel Zotti <daniel.zotti87@gmail.com>
2026-05-06 12:57:45 +0200

    Add border radius and width inputs to widget image component

--- src/app/components/widget/widget-image/widget-image.component.ts
+++ src/app/components/widget/widget-image/widget-image.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject, input} from '@angular/core';
 import {WidgetImageContent} from '../../../models/canvas-widget-state.models';
 import {CanvasService} from '../../../services/canvas.service';
 
@@ -8,11 +8,22 @@ import {CanvasService} from '../../../services/canvas.service';
   templateUrl: './widget-image.component.html',
   styleUrl: './widget-image.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[style.border-radius.px]': 'innerBorderRadius()',
+    '[style.overflow]': '"hidden"',
+  },
 })
 export class WidgetImageComponent {
   private readonly canvasService = inject(CanvasService);
 
   content = input.required<WidgetImageContent>();
+  borderRadius = input(0);
+  borderWidth = input(0);
+
+  /** Inner border-radius = max(0, borderRadius - borderWidth) to follow the widget border curve. */
+  protected readonly innerBorderRadius = computed(() =>
+    Math.max(0, this.borderRadius() - this.borderWidth()),
+  );
 
   protected get isUrlValid(): boolean {
     return this.canvasService.isValidImageUrl(this.content().src);
